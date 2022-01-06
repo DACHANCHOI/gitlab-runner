0c12354b49504165bdfc33c50045249da5384acc
DACHANCHOI <dachan1207@gmail.com>
2022-01-06 18:27:05 +0900

    test

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
+apt-get update && apt-get install -y ca-certificates curl gnupg lsb-release unzip
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update && apt-get install -y apt-transport-https \
-       ca-certificates curl gnupg2 \
-       software-properties-common
-
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-       "deb [arch=amd64] https://download.docker.com/linux/debian \
-       $(lsb_release -cs) stable"
-
-apt-get update && apt-get install -y docker-ce-cli
+apt update && apt-get install docker-ce-cli
