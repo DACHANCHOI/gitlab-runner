46663de386b5ae9e3dd4f679000c34c8892cbdab
DACHANCHOI <dachan1207@gmail.com>
2022-01-06 18:30:14 +0900

    test

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -4,4 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update && apt-get install docker-ce-cli
+apt update && apt-get install -y docker-ce-cli
