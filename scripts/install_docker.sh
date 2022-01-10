e19fbb450860bbaa4882872d1a4e3a7419ae365b
DACHANCHOI <dachan1207@gmail.com>
2022-01-10 13:30:10 +0900

    test

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -5,3 +5,4 @@ echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/doc
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update && apt-get install -y docker-ce-cli
+apt-get install -y jq
