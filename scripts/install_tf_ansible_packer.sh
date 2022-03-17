45fe445ac61a70c8bd18a9b1d6264239c19399a1
DACHANCHOI <dachan1207@gmail.com>
2022-03-17 14:03:55 +0900

    test

--- scripts/install_tf_ansible_packer.sh
+++ scripts/install_tf_ansible_packer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # terraform install
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && sudo apt-get install terraform
+apt-get update && apt-get install terraform
 
 # packer install
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update && sudo apt-get install packer
+apt-get update && apt-get install packer
 
 # ansible install
 apt update
